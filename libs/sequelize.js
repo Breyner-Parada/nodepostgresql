089b11c80119f25afe8876e3e1db973da3a85f88
Breyner-Parada <paradabre@gmail.com>
2022-07-08 18:02:13 -0500

    fix error in connection

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -5,9 +5,16 @@ const setupModels = require('../db/models');
 
 const options = {
   dialect: 'postgres',
-  logging: config.isProd ? true : false
+  logging: config.isProd ? false : true
 }
 
+if (config.isProd) {
+  options.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+}
 
 const sequelize = new Sequelize(config.dbUrl, options);
 
